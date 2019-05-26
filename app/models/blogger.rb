0ec4e4c6496659fd537c13cc275b9f8cbcea62f9
askoenig <askoenig94@gmail.com>
2019-05-26 17:00:11 -0400

    testing

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,7 @@
 class Blogger < ApplicationRecord
+  has_many :posts
+  has_many :destinations, through: :posts
 
+  validates_uniqueness_of :name
+  validates_length_of :bio, minimum: 30, maximum: 10000
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
+  belongs_to :blogger
+  belongs_to :destination
+
+  validates_length_of :content, minimum: 100
 
 end
